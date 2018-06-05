072da47221bd9b8f410136ddca06e4433e408d9e
Jag Talon <jag@jagtalon.com>
2018-06-05 14:32:38 -0400

    Add close button
    Fixes #4

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,8 +1,25 @@
 $(function() {
+    var $body = $('body');
     var $sidemenu = $('.side-menu');
     var $sidebar = $('.side-bar');
+    var $sideclose = $('.side-bar-close');
+    var $sidenav = $('.side-nav')
 
+    // Show the sidemenu when the hamburger menu is clicked.
     $sidemenu.click(function() {
         $sidebar.addClass('show');
+    });
+
+    // Hide the sidemenu when the X button is clicked/
+    $sideclose.click(function() {
+        $sidebar.removeClass('show');
+    });
+
+    // When we click outside the sidemenu, we close it, too.
+    $body.click(function(el) {
+        if(!$.contains($sidebar.get(0), el.target) &&
+           !$.contains($sidenav.get(0), el.target)) {
+            $sidebar.removeClass('show');
+        }
     });
 });
